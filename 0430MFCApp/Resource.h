e36fbaf6baf01799d186ffd715e8ebea98d86744
kevin0047 <ybnam0047@gmail.com>
2025-05-05 09:19:45 +0900

    아이콘 변경

--- 0430MFCApp/Resource.h
+++ 0430MFCApp/Resource.h
@@ -1,9 +1,9 @@
 ﻿//{{NO_DEPENDENCIES}}
 // Microsoft Visual C++에서 생성한 포함 파일입니다.
-// My0430MFCApp.rc에서 사용
+// My0430MFCApp.rc에서 사용되고 있습니다.
 //
-#define IDR_MAINFRAME                   128
 #define IDD_MY0430MFCAPP_DIALOG         102
+#define IDR_MAINFRAME                   128
 #define IDC_LIST_INDICATORS             1000
 #define IDC_EDIT_DELAY                  1001
 #define IDC_BUTTON_CONNECT              1002
@@ -12,19 +12,18 @@
 #define IDC_BUTTON_STOP_POLLING         1005
 #define IDC_LIST_LOG                    1006
 #define IDC_BUTTON_CLEAR_LOG            1007
-#define IDC_EDIT_PLC_IP                  1008
+#define IDC_EDIT_PLC_IP                 1008
 #define IDC_EDIT_PLC_PORT               1009
 #define IDC_BUTTON_CONNECT_PLC          1010
-#define IDC_STATIC_PLC_STATUS			1011
+#define IDC_STATIC_PLC_STATUS           1011
 
-// 다음은 새 개체에 사용할 기본값입니다.
-//
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1008
 #define _APS_NEXT_SYMED_VALUE           101
-#define _APS_NEXT_COMMAND_VALUE         32771
 #endif
 #endif
-
